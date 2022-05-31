60aa478d4ed73002165a6d470d1e48604daae36a
imrantahir16 <digitronicdev@gmail.com>
2022-05-31 20:26:34 +0500

    Added code for firebase authentication

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
+// import NotFoundPage from './pages/NotFoundPage';
+
+function App() {
+  const authctx = useContext(AuthContext);
+  const isLoaggedIn = authctx.isLoggedIn;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" end element={<HomePage />} />
+        {!isLoaggedIn && <Route path="/auth" element={<AuthPage />} />}
+        <Route
+          path="/profile"
+          element={
+            (isLoaggedIn && <UserProfile />) ||
+            (!isLoaggedIn && <Navigate to="/auth" replace />)
+          }
+        />
+        <Route path="*" element={<HomePage />} />
+        {/* <Route path="*" element={<NotFoundPage />} /> we could use 404 page here */}
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,104 @@
+import { useContext, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const navigate = useNavigate();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const authCtx = useContext(AuthContext);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    let url;
+    setIsLoading(true);
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCRD3sXP3Jo_jAU4itLuf_pm5AnHcYrmdA";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCRD3sXP3Jo_jAU4itLuf_pm5AnHcYrmdA";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        // console.log(data);
+        authCtx.login(data.idToken);
+        navigate("/", { replace: true });
+      })
+      .catch((err) => {
+        // console.error(err);
+        alert(err.message);
+      });
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
